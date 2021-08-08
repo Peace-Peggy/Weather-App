2f04a2ddf12fcb1065334101b6f80d777a1c2630
Peggy <peacepeggyabua@gmail.com>
2021-08-08 13:19:14 -0500

    Adjusted styling in CSS. Created variable placeholders in JS

--- src/script.js
+++ src/script.js
@@ -1,4 +1,3 @@
-//VANILLA JS
 function formatDateTime(timestamp){
   //calculate the time and date. Then return a formated date and time
   let date    = new Date(timestamp);
@@ -24,63 +23,72 @@ function formatDateTime(timestamp){
   return `${day} ${hours}:${minutes}`
 
 }
+function formatForecastDay(timestamp){
+  let date = new Date(timestamp * 1000);
+  let day  = date.getDay();
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+  return days[day];
+  
+}
 function displayWeatherForecast(response){
-  console.log(response.data.daily);
+  let forecast     = response.data.daily;
   let holdForecast = document.querySelector("#days-forecast");
 
-  let getForecast = `<div class="row">`;
-
-  let days = ["Thu", "Fri", "Sat", "Sun"];
+  let getForecastHTML = `<div class="row">`;
   
-  days.forEach(function(day){
-      getForecast = 
-      getForecast + `
+  forecast.forEach(function(getForecastDay, index){
+    if(index < 6){
+    let icon    = getForecastDay.weather[0].icon;
+    let maxTemp = Math.round(getForecastDay.temp.max);
+    let minTemp = Math.round(getForecastDay.temp.min);
+    
+      getForecastHTML = 
+      getForecastHTML + `
           <div class="col-2">
             <div class="forecast-day">
-              ${day}
+              ${formatForecastDay(getForecastDay.dt)}
             </div>
-            <img src="https://ssl.gstatic.com/onebox/weather/64/fog.png" alt="" width="38" />
+            <img src="http://openweathermap.org/img/wn/${icon}@2x.png" width="40" />
             <div class="forecast-temperature">
-              <span ="forecast-in-max">20</span>
-              <span ="forecast-in-min">68</span>
+              <span ="forecast-in-max">${maxTemp}°</span>
+              <span ="forecast-in-min">${minTemp}°</span>
             </div>
           </div>`;
+          }
          });
-      getForecast            = getForecast + `</div>`;
-      holdForecast.innerHTML = getForecast;
-
-}
+      getForecastHTML        = getForecastHTML + `</div>`;
+      holdForecast.innerHTML = getForecastHTML
+      }
 
 function getWeatherForecast(cordinates){
-  console.log(cordinates);
   let latitude    = cordinates.lat;
   let longitude   = cordinates.lon;
   let apiKey      = "76408f461806bdd0e29fa34c52cb5991";
   let forecastURL = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
   axios.get(forecastURL).then(displayWeatherForecast);
-  console.log(forecastURL);
 }
 function displayTemperature(response){
-        celciusTemp                  = response.data.main.temp;
-    let iconElement                  = document.querySelector("#icon");
-    let getCityName                  = response.data.name;
-    let getWeatherDescription        = response.data.weather[0].description;
-    let getHumidity                  = response.data.main.humidity;
-    let getWind                      = Math.round(response.data.wind.speed);
-    let cityName                     = document.querySelector("#city");
-        cityName.innerHTML           = getCityName;
-    let temperature                  = document.querySelector("#temperature");
-    let holdTempValue                = Math.round(celciusTemp);
-        temperature.innerHTML        = holdTempValue;
-    let weatherDescription           = document.querySelector("#weather-description");
-        weatherDescription.innerHTML = getWeatherDescription;
-    let humidity                     = document.querySelector("#humidity");
-        humidity.innerHTML           = getHumidity;
-    let wind                         = document.querySelector("#wind");
-        wind.innerHTML               = getWind;
-    let dateElement                  = document.querySelector("#date-time");
-        dateElement.innerHTML        = formatDateTime(response.data.dt*1000);
-    let getIconCode                  = response.data.weather[0].icon;
+                                                                                                                celciusTemp                  = response.data.main.temp;
+                                                                                                            let iconElement                  = document.querySelector("#icon");
+                                                                                                            let getCityName                  = response.data.name;
+                                                                                                            let getWeatherDescription        = response.data.weather[0].description;
+                                                                                                            let getHumidity                  = response.data.main.humidity;
+                                                                                                            let getWind                      = Math.round(response.data.wind.speed);
+                                                                                                            let cityName                     = document.querySelector("#city");
+                                                                                                                cityName.innerHTML           = getCityName;
+                                                                                                            let temperature                  = document.querySelector("#temperature");
+                                                                                                            let holdTempValue                = Math.round(celciusTemp);
+                                                                                                                temperature.innerHTML        = holdTempValue;
+                                                                                                            let weatherDescription           = document.querySelector("#weather-description");
+                                                                                                                weatherDescription.innerHTML = getWeatherDescription;
+                                                                                                            let humidity                     = document.querySelector("#humidity");
+                                                                                                                humidity.innerHTML           = getHumidity;
+                                                                                                            let wind                         = document.querySelector("#wind");
+                                                                                                                wind.innerHTML               = getWind;
+                                                                                                            let dateElement                  = document.querySelector("#date-time");
+                                                                                                                dateElement.innerHTML        = formatDateTime(response.data.dt*1000);
+                                                                                                            let getIconCode                  = response.data.weather[0].icon;
 iconElement.setAttribute("src", `http://openweathermap.org/img/wn/${getIconCode}@2x.png`);
 iconElement.setAttribute("alt", `${getWeatherDescription}`)
 
