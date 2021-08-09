0d9c6d52ccdd603561f4a81f66024b628a0c2544
Peggy <peacepeggyabua@gmail.com>
2021-08-09 18:50:29 -0500

    Rearranged code in showCelcius and showFahrenheit function

--- src/script.js
+++ src/script.js
@@ -69,26 +69,26 @@ function getWeatherForecast(cordinates){
   axios.get(forecastURL).then(displayWeatherForecast);
 }
 function displayTemperature(response){
-                                                                                                                    celciusTemp                  = response.data.main.temp;
-                                                                                                                let iconElement                  = document.querySelector("#icon");
-                                                                                                                let getCityName                  = response.data.name;
-                                                                                                                let getWeatherDescription        = response.data.weather[0].description;
-                                                                                                                let getHumidity                  = response.data.main.humidity;
-                                                                                                                let getWind                      = Math.round(response.data.wind.speed);
-                                                                                                                let cityName                     = document.querySelector("#city");
-                                                                                                                    cityName.innerHTML           = getCityName;
-                                                                                                                let temperature                  = document.querySelector("#temperature");
-                                                                                                                let holdTempValue                = Math.round(celciusTemp);
-                                                                                                                    temperature.innerHTML        = holdTempValue;
-                                                                                                                let weatherDescription           = document.querySelector("#weather-description");
-                                                                                                                    weatherDescription.innerHTML = getWeatherDescription;
-                                                                                                                let humidity                     = document.querySelector("#humidity");
-                                                                                                                    humidity.innerHTML           = getHumidity;
-                                                                                                                let wind                         = document.querySelector("#wind");
-                                                                                                                    wind.innerHTML               = getWind;
-                                                                                                                let dateElement                  = document.querySelector("#date-time");
-                                                                                                                    dateElement.innerHTML        = formatDateTime(response.data.dt*1000);
-                                                                                                                let getIconCode                  = response.data.weather[0].icon;
+                                                                                                                                                    celciusTemp                  = response.data.main.temp;
+                                                                                                                                                let iconElement                  = document.querySelector("#icon");
+                                                                                                                                                let getCityName                  = response.data.name;
+                                                                                                                                                let getWeatherDescription        = response.data.weather[0].description;
+                                                                                                                                                let getHumidity                  = response.data.main.humidity;
+                                                                                                                                                let getWind                      = Math.round(response.data.wind.speed);
+                                                                                                                                                let cityName                     = document.querySelector("#city");
+                                                                                                                                                    cityName.innerHTML           = getCityName;
+                                                                                                                                                let temperature                  = document.querySelector("#temperature");
+                                                                                                                                                let holdTempValue                = Math.round(celciusTemp);
+                                                                                                                                                    temperature.innerHTML        = holdTempValue;
+                                                                                                                                                let weatherDescription           = document.querySelector("#weather-description");
+                                                                                                                                                    weatherDescription.innerHTML = getWeatherDescription;
+                                                                                                                                                let humidity                     = document.querySelector("#humidity");
+                                                                                                                                                    humidity.innerHTML           = getHumidity;
+                                                                                                                                                let wind                         = document.querySelector("#wind");
+                                                                                                                                                    wind.innerHTML               = getWind;
+                                                                                                                                                let dateElement                  = document.querySelector("#date-time");
+                                                                                                                                                    dateElement.innerHTML        = formatDateTime(response.data.dt*1000);
+                                                                                                                                                let getIconCode                  = response.data.weather[0].icon;
 iconElement.setAttribute("src", `http://openweathermap.org/img/wn/${getIconCode}@2x.png`);
 iconElement.setAttribute("alt", `${getWeatherDescription}`)
 
@@ -110,18 +110,18 @@ function submitForm(event){
 
 function showFahrenheit(event) {
   event.preventDefault();
-  let theTemperature = document.querySelector("#temperature");
-  linkToCelcius.classList.remove("active");
-  linkToFahrenheit.classList.add("active");
+  let theTemperature           = document.querySelector("#temperature");
   let fahrenheitConversion     = (celciusTemp * 9) / 5 + 32;
       theTemperature.innerHTML = Math.round(fahrenheitConversion);
+      linkToCelcius.classList.remove("active");
+      linkToFahrenheit.classList.add("active");
 }
 function showCelcius(event) {
   event.preventDefault();
-  linkToCelcius.classList.add("active");
-  linkToFahrenheit.classList.remove("active");
   let theTemperature           = document.querySelector("#temperature");
       theTemperature.innerHTML = Math.round(celciusTemp);
+        linkToCelcius.classList.add("active");
+  linkToFahrenheit.classList.remove("active");
 }
 
 let celciusTemp = null;
